d0c10fcf37037e1b12ae926e85cfbbd693f85a0f
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2026-02-23 12:21:58 +0700

    refactor: move prisma client to src/db

---
+++ src/db/prisma.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+// В dev-режиме защищаемся от hot-reload создания новых клиентов
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
